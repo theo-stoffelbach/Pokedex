217de0be824f26da402cb6dee1a095af92bb6784
Jordan20031 <79769661+Jordan20031@users.noreply.github.com>
2021-11-25 17:48:57 +0100

    push 1.1

--- TEST-serveur/serveur.js
+++ TEST-serveur/serveur.js
@@ -1,12 +1,18 @@
 var express = require('express'),
-app = express(),
-port = process.env.PORT || 3000,
-mongoose = require('mongoose'),
-User = require('./api/models/userListModels'),
-bodyParser = require('body-parser');
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    User = require('./api/models/userListModels'),
+    bodyParser = require('body-parser');
 
 
 mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/users');
+app.use(bodyParser.urlencoded({ extented: true }));
+app.use(bodyParser.json());
+
+var route = require('./api/routes/userListRoutes');
+routes(app);
 app.listen(port);
 console.log('user List started on:' + port);
 
