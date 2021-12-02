2f2f68f2ffe5a523ddfd5aca1420eb8424d6dd8d
Jordan20031 <79769661+Jordan20031@users.noreply.github.com>
2021-12-02 09:37:35 +0100

    push 1

--- mern-project/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-require('dotenv').config({path:'./config/.env'});
-require('./config/db');
-const app = express();
-
-app.listen(process.env.PORT,() =>{
-    console.log(`listening on port ${process.env.PORT}`);
-})
