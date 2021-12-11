74847313d3726a31afa67507971eaac96cbaa937
Florian <74573533+UtokaSan@users.noreply.github.com>
2021-12-11 17:05:11 +0100

    Update team

--- FrontEnd/src/views/Team.vue
+++ FrontEnd/src/views/Team.vue
@@ -52,8 +52,6 @@ body {
     margin-left: 15em;
 }
 .text_pika {
-    background-image: url("../assets/background_pika.png");
-    background-size: 50%;
     font-size: 30px;
     height: 70px;
     border: yellow 2px solid;
@@ -64,30 +62,40 @@ body {
 .text_charm {
     border: red 2px solid;
     border-radius: 30px;
+    padding-left: 50px;
+    padding-top: 30px;
     font-size: 30px;
     height: 70px;
 }
 .text_pidgey {
     border: grey 2px solid;
-    border-radius: 30px; 
+    border-radius: 30px;
+    padding-left: 50px;
+    padding-top: 30px; 
     font-size: 30px;
     height: 70px;
 }
 .text_bulb {
     border: green 2px solid;
     border-radius: 30px;
+    padding-left: 50px;
+    padding-top: 30px;
     font-size: 30px;
     height: 70px;
 }
 .text_squir {
     border: blue 2px solid;
     border-radius: 30px;
+    padding-left: 50px;
+    padding-top: 30px;
     font-size: 30px;
     height: 70px;
 }
 .text_blast {
     border: #3b147b 2px solid;
     border-radius: 30px;
+    padding-left: 50px;
+    padding-top: 30px;
     font-size: 30px;
     height: 70px;
 }
@@ -96,8 +104,6 @@ body {
     position: relative;
     right: -280px;
     top: 20px;
-}
-img {
     width: 90px;
 	height: 90px;
 }
