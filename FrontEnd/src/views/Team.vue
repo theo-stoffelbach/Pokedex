22862e5cde0c4b9928272fea463417109a64768d
Florian <74573533+UtokaSan@users.noreply.github.com>
2021-12-11 18:44:37 +0100

    Update Team

--- FrontEnd/src/views/Team.vue
+++ FrontEnd/src/views/Team.vue
@@ -28,24 +28,23 @@ body {
 }
 #all_txt {
    border: red 2px solid;
-   max-width: 1550px;
+   max-width: 300px;
+   display: grid;
+   margin-top: 6em;
+   grid-template-columns: 300px 300px;
+   grid-gap: 450px;
+   margin-left : 15em;
+   margin-right: 15em;
    width: auto;
 }
 /* change opacity for the background*/
 .first_text { 
-    margin-top: 6em;
-    display: flex;
-    width: 300px;
-    flex-direction: column;
-    margin-right: 15em; 
     color: #f88;
+    width: 250px;
 }
 .second_text {
     color: #f88;
-    margin-top: 5em;
-    display: flex;
-    flex-direction: column;
-    margin-left: 15em;
+    width: 250px;
 }
 .text_pika {
     font-size: 30px;
