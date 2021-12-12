bac76715108bc6ab31898b1cbbd222fba9038ad8
Jordan20031 <79769661+Jordan20031@users.noreply.github.com>
2021-12-12 23:38:17 +0100

    123

--- pokedex/employeemanager/src/components/Dashboard.vue
+++ pokedex/employeemanager/src/components/Dashboard.vue
@@ -14,26 +14,26 @@
     import db from './firebaseInit'
     export default{
         name: "dashboard",
-        data (){
-            return {
-                employees: []
-            }
-             
-            },
-            created (){
-                db.collection("employees").get().then(querySnapshot => {
-                    querySnapshot.forEach(doc =>{
-                        const data ={
-                            'id': doc.id,
-                            'employee_id': doc.data().employee_id,
-                            'name': doc.data().name,
-                            'dept':doc.data().dept,
-                            'position':doc.data().position
-
-                        }
-                    })
-                })
-            }
-            
-}
+        data () {
+      return {
+        employees: [],
+        loading: true
+      }
+    },
+    created () {
+      db.collection('employees').orderBy('dept').get().then((querySnapshot) => {
+        this.loading = false
+        querySnapshot.forEach((doc) => {
+          const data = {
+            'id': doc.id,
+            'employee_id': doc.data().employee_id,
+            'name': doc.data().name,
+            'dept': doc.data().dept,
+            'position': doc.data().position
+          }
+          this.employees.push(data)
+        })
+      })
+    }
+  }
 </script>
--- pokedex/employeemanager/src/components/firebaseConfig.js
+++ pokedex/employeemanager/src/components/firebaseConfig.js
@@ -1,5 +1,5 @@
 export default {
-    apiKey: "AIzaSyBZqiKxOaKx5oE02MGxUEpjxj0n8CxhhLQ",
+  apiKey: "AIzaSyBZqiKxOaKx5oE02MGxUEpjxj0n8CxhhLQ",
   authDomain: "pokedex-b1691.firebaseapp.com",
   databaseURL: "https://pokedex-b1691-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "pokedex-b1691",
@@ -7,4 +7,5 @@ export default {
   messagingSenderId: "760827219857",
   appId: "1:760827219857:web:52f2d61462cb3b41fa9430",
   measurementId: "G-9FF4D7NFS1"
+  
 }
